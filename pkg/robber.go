cccbc115eed07a1599250d242271177dfb06387a
Amir Mofasser <amir.mofasser@gmail.com>
2019-06-13 14:04:11 +0200

    Added package robber

---
+++ pkg/robber.go
@@ -0,0 +1,38 @@
+package robber
+
+import (
+	"fmt"
+	"strings"
+)
+
+var consonants = []rune{'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'z', 'w'}
+
+func isInArr(s rune, a []rune) bool {
+	for _, v := range a {
+		if s == v {
+			return true
+		}
+	}
+	return false
+}
+
+// Encode translates a string into robber languange
+func Encode(s string) string {
+	var r string
+	for _, v := range strings.ToLower(s) {
+		if isInArr(v, consonants) {
+			r = r + fmt.Sprintf("%co%c", v, v)
+			continue
+		}
+		r = r + fmt.Sprintf("%c", v)
+	}
+	return r
+}
+
+// Decode translates tries to reverse robber languange into ordinary languange
+func Decode(s string) string {
+	for _, v := range consonants {
+		s = strings.Replace(strings.ToLower(s), fmt.Sprintf("%co%c", v, v), string(v), -1)
+	}
+	return s
+}
---
+++ pkg/robber_test.go
@@ -0,0 +1,23 @@
+package robber
+
+import (
+	"testing"
+)
+
+func TestEncode(t *testing.T) {
+	str := "Ibland måste man göra saker man inte vågar, annars är man ingen människa utan bara en liten lort"
+	expected := "iboblolanondod momåsostote momanon gogörora sosakokeror momanon inontote vovågogaror, anonnonarorsos äror momanon inongogenon momänonnonisoskoka utotanon bobarora enon lolitotenon lolorortot"
+	encoded := Encode(str)
+	if encoded != expected {
+		t.Fatalf("Expected '%s' but got '%s'", expected, encoded)
+	}
+}
+
+func TestDecode(t *testing.T) {
+	encoded := "hohejoj jojagog hohetoteror amomiror"
+	expected := "hej jag heter amir"
+	decoded := Decode(encoded)
+	if decoded != expected {
+		t.Fatalf("Expected '%s' but got '%s'", expected, decoded)
+	}
+}
