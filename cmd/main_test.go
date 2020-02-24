ec42da642e25758f24a6b6bec4a5247106be9a7c
Amir Mofasser <amir.mofasser@gmail.com>
2020-02-24 10:30:59 +0100

    Added unit test for `usage()` func

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestUsageText(t *testing.T) {
+	expected := "Usage:\n  robberlang [-d] <string>\n\n"
+	if usage() != expected {
+		t.Fatalf("Usage text mismatch, got %s expected %s", usage(), expected)
+	}
+}
